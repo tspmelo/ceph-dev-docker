f9ef064b6840cb02a069d5adf0c918108fc6c7ca
Tiago Melo <tmelo@suse.com>
2020-07-21 22:05:06 +0000

    Manually set the proxy port number
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- shared/bin/npm-start.sh
+++ shared/bin/npm-start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+##
+# You can pass a port number and it will be used in the proxy
+# npm-start.sh 8443
+##
+
 set -e
 
-setup-proxy.sh
+setup-proxy.sh $1
 
 cd /ceph/src/pybind/mgr/dashboard/frontend
 source /ceph/build/src/pybind/mgr/dashboard/node-env/bin/activate
--- shared/bin/setup-proxy.sh
+++ shared/bin/setup-proxy.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+##
+# You can pass a port number and it will be used in the proxy
+# setup-proxy.sh 8443
+##
+
 set -e
 
-cd /ceph/build
-url=`./bin/ceph mgr services | jq .dashboard`
+
+if [[ -z "$1" ]]; then
+  cd /ceph/build
+  url=`./bin/ceph mgr services | jq .dashboard`
+else
+  url="\"https://localhost:$1/\""
+fi
 
 cd /ceph/src/pybind/mgr/dashboard/frontend
 jq '.["/api/"].target'=$url proxy.conf.json.sample | jq '.["/ui-api/"].target'=$url > proxy.conf.json
